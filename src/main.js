990a113b7f5b61a26be88baf05fdd3279822d56d
Horatzio <horatiu.duma@luminoslabs.com>
2021-02-15 23:09:28 +0200

    Installed Vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import vuetify from '@/plugins/vuetify'
 
 Vue.config.productionTip = false
 
 Vue.component('hello-world', require('./components/HelloWorld.vue').default);
 
 window.Vue = Vue;
+
+new Vue({
+    vuetify
+}).$mount('#app');
