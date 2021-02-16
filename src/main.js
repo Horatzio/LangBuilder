887993d8494a4c15291ef2b6f17c6baac954cd7d
Horatzio <horatiu.duma@luminoslabs.com>
2021-02-16 20:51:48 +0200

    Installed Vue.Draggable, vue anime.js and Vue Kinesis.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import vuetify from '@/plugins/vuetify'
+import VueAnime from 'vue-animejs'
 
 Vue.config.productionTip = false
+Vue.use(VueAnime);
 
 Vue.component('hello-world', require('./components/HelloWorld.vue').default);
+Vue.component('test-list', require('./components/TestList.component.vue').default);
 
 window.Vue = Vue;
 
