2356bb47563c61e2eece40644abc3b5f99b412e5
Horatzio <horatiuduma@gmail.com>
2023-01-04 12:46:33 +0200

    Final changes

--- src/react/langbuilder/src/components/transpiler-runner/CodeInput.tsx
+++ src/react/langbuilder/src/components/transpiler-runner/CodeInput.tsx
@@ -31,52 +31,7 @@ const CodeInput: React.FC<CodeInputProps> = ({ value, onChange }) => {
             </div>
             <div className="border-t border-gray-500/30 font-mono text-gray-200 text-xs p-4 space-y-2">
               <h3>Problems</h3>
-              <ul className="text-gray-300 space-y-1">
-                <li className="flex space-x-2 items-center">
-                  <svg
-                    width="24"
-                    height="24"
-                    fill="none"
-                    className="flex-none text-yellow-400"
-                  >
-                    <path
-                      d="m5.207 16.203 5.072-10.137c.711-1.422 2.736-1.421 3.447 0l5.067 10.137c.642 1.285-.29 2.797-1.723 2.797H6.93c-1.434 0-2.366-1.513-1.723-2.797ZM12 10v2"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                    <path
-                      d="M12.5 16a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z"
-                      stroke="currentColor"
-                    ></path>
-                  </svg>
-                  <p>'flex' applies the same CSS property as 'block'.</p>
-                  <p className="text-gray-500">cssConflict [1, 20]</p>
-                </li>
-                <li className="flex space-x-2 items-center">
-                  <svg
-                    width="24"
-                    height="24"
-                    fill="none"
-                    className="flex-none text-yellow-400"
-                  >
-                    <path
-                      d="m5.207 16.203 5.072-10.137c.711-1.422 2.736-1.421 3.447 0l5.067 10.137c.642 1.285-.29 2.797-1.723 2.797H6.93c-1.434 0-2.366-1.513-1.723-2.797ZM12 10v2"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                    <path
-                      d="M12.5 16a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z"
-                      stroke="currentColor"
-                    ></path>
-                  </svg>
-                  <p>'block' applies the same CSS property as 'flex'.</p>
-                  <p className="text-gray-500">cssConflict [1, 54]</p>
-                </li>
-              </ul>
+              <ul className="text-gray-300 space-y-1"></ul>
             </div>
           </div>
         </div>
@@ -86,3 +41,13 @@ const CodeInput: React.FC<CodeInputProps> = ({ value, onChange }) => {
 };
 
 export default CodeInput;
+
+/*
+construct Solution
+<
+  operation Main {text[] args}
+  <
+    display["HelloWorld"]
+  >
+>
+*/
